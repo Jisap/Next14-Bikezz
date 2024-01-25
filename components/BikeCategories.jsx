53fec2e89f9b9e859fe07b98c5b61b4fbafc91a8
Jorge Matos <jisapjisap@gmail.com>
2024-01-25 18:57:34 +0100

    filtered bikes

--- components/BikeCategories.jsx
+++ components/BikeCategories.jsx
@@ -1,7 +1,7 @@
 "use client"
 
 import { useState, useEffect } from 'react';
-import { RadioGroup, RadioGroupItem} from '@/components/ui/radio-group'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
 import { Label } from './ui/label';
 import { Slider } from './ui/slider';
 import Bike from './Bike';
@@ -12,8 +12,19 @@ const BikeCategories = ({ bikes }) => {
 
   const [category, setCategory] = useState('all');
   const [filteredBikes, setFilteredBikes] = useState([]);
-  const [price , setPrice] = useState(900)
-console.log(category)
+  const [price, setPrice] = useState(900)
+  
+  useEffect(() => {
+    const filtered = bikes.filter((bike) => {
+      const categoryMatch = category === 'all'
+        ? bikes
+        : bike.categories.some((categ) => categ.name === category)
+      const priceMatch = bike.price <= price;
+      return categoryMatch && priceMatch
+    });
+    setFilteredBikes(filtered)
+  },[category, price, bikes])
+console.log(filteredBikes)
   return (
     <section className='min-h-[1200px] py-10'>
       <div className='container mx-auto'>
@@ -22,22 +33,58 @@ console.log(category)
           <aside className='bg-yellow-200 w-full p-4 mb-8 xl:w-[300px] xl:h-[84vh] xl:fixed'>
             <RadioGroup defaultValue="all" className="flex flex-col gap-6 mb-12">
               <div className='flex items-center space-x-2'>
-                <RadioGroupItem value='all' id='all' onClick={() => setCategory('all')} />
+                <RadioGroupItem
+                  value='all'
+                  id='all'
+                  onClick={() => setCategory('all')}
+                />
                 <label htmlFor="all">All</label>
               </div>
               <div className='flex items-center space-x-2'>
-                <RadioGroupItem value='road' id='road' onClick={() => setCategory('road')} />
+                <RadioGroupItem 
+                  value='road' 
+                  id='road' 
+                  onClick={() => setCategory('road')}
+                />
                 <label htmlFor="road">Road</label>
               </div>
               <div className='flex items-center space-x-2'>
-                <RadioGroupItem value='professional' id='professional' onClick={() => setCategory('professional')} />
+                <RadioGroupItem 
+                  value='professional' 
+                  id='professional' 
+                  onClick={() => setCategory('professional')} 
+                />
                 <label htmlFor="professional">Professional</label>
               </div>
               <div className='flex items-center space-x-2'>
-                <RadioGroupItem value='extreme' id='extreme' onClick={() => setCategory('extreme')} />
+                <RadioGroupItem 
+                  value='extreme' 
+                  id='extreme' 
+                  onClick={() => setCategory('extreme')} 
+                />
                 <label htmlFor="extreme">Extreme</label>
               </div>
             </RadioGroup>
+            {/* price slider */}
+            <div className='max-w-56'>
+              <div className='text-lg mb-4 font-medium'>
+                Max Price: <span className='text-accent font-semibold ml-2'>${price}</span>
+                <span className='ml-2'>
+                  {filteredBikes.length > 1 
+                    ? `${filteredBikes.length} items`
+                    : filteredBikes === 0 
+                      ? `${filteredBikes.length} items`
+                      : `${filteredBikes.length} item` 
+                  }
+                </span>
+              </div>
+              <Slider 
+                defaultValue={[900]}
+                max={1000}
+                step={1}
+                onValueChange={(val) => setPrice(val[0])}
+              />
+            </div>
           </aside>
           {/* bike list */}
           <div className='bg-blue-300 w-full xl:w-[1050px] ml-auto'>
