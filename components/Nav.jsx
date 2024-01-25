53097aa4b1296aa6e34ba54d2842ec05bcdb5f5d
Jorge Matos <jisapjisap@gmail.com>
2024-01-25 17:21:58 +0100

    Pruebas en CheckServices

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -9,11 +9,18 @@ const links = [
 ]
 
 const Nav = ({ containerStyles }) => {
+
+  const pathname = usePathname();
+
   return (
     <nav className={`${containerStyles}`}>
       {links.map((link, index) => {
         return (
-          <Link href={link.path} key={index}>
+          <Link 
+            href={link.path} 
+            key={index}
+            className={`${link.path === pathname && 'text-accent'}`}  
+          >
             {link.name}
           </Link>
         )
