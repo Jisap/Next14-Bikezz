f46146eece42a284bb1c5d247350c80f1e2fc5f0
Jorge Matos <jisapjisap@gmail.com>
2024-01-27 09:31:21 +0100

    success page

--- app/stripe/success/page.jsx
+++ app/stripe/success/page.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
+import Link from "next/link"
+
 
 const SuccessPage = () => {
   return (
-    <div>SuccessPage</div>
+    <section className="py-72">
+      <div className="container mx-auto">
+        <h3 className="text-center mb-4">Your payment was successfull! Thank you!</h3>
+        <Link href="/">
+          <button className="btn btn-primary mx-auto">Back to the homepage</button>
+        </Link>
+      </div>
+    </section>
   )
 }
 
