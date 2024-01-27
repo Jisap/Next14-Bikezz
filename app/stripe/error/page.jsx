b2831c1833e524150668670e1f26d071d9ad0308
Jorge Matos <jisapjisap@gmail.com>
2024-01-27 09:33:34 +0100

    error page

--- app/stripe/error/page.jsx
+++ app/stripe/error/page.jsx
@@ -1,8 +1,17 @@
-import React from 'react'
+import Link from "next/link"
+
+
 
 const ErrorPage = () => {
   return (
-    <div>ErrorPage</div>
+    <section className="py-72">
+      <div className="container mx-auto">
+        <h3 className="text-center mb-4">Something went wrong!</h3>
+        <Link href="/">
+          <button className="btn btn-accent mx-auto">Back to the homepage</button>
+        </Link>
+      </div>
+    </section>
   )
 }
 
